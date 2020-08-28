5323df726b0b6ed29d1548c0f7b449b1a09897bd
Ramon <monchouam@gmail.com>
2020-08-28 16:41:29 -0500

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -25,7 +25,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://moncho88vivi:r4UBzsmLzgJZCJed@cluster0.mew8g.mongodb.net/cafe';
+    //urlDB = 'mongodb+srv://moncho88vivi:r4UBzsmLzgJZCJed@cluster0.mew8g.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB; //.URLDB es un nombre inventado
